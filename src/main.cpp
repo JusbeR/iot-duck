c9264b48f578fd9e5b6f903646f9eeea89a69f08
Jussi Ratilainen <jussi.t.ratilainen@gmail.com>
2019-08-29 22:06:46 +0300

    Combine display/temp readings

--- src/main.cpp
+++ src/main.cpp
@@ -6,54 +6,88 @@
  */
 #include "Arduino.h"
 #include "heltec.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
+#define ONE_WIRE_BUS 17
+#define NO_OF_TEMP_SENSORS 4
+
+float temperatures[NO_OF_TEMP_SENSORS];
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void printAddress(DeviceAddress deviceAddress) {
+  for (uint8_t i = 0; i < 8; i++){
+    if (deviceAddress[i] < 16) Serial.print("0");
+      Serial.print(deviceAddress[i], HEX);
+  }
+}
 
 void setup()
 {
-  // initialize LED digital pin as an output.
-  //pinMode(LED_BUILTIN, OUTPUT);
+  for(int i=0; i<NO_OF_TEMP_SENSORS; i++) { temperatures[i] = 0; }
+  sensors.begin();
   Serial.begin(9600);
   Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, false /*Serial Enable*/);
 
-
-
   Heltec.display->flipScreenVertically();
-Heltec.display->setFont(ArialMT_Plain_10);
+  Heltec.display->setFont(ArialMT_Plain_10);
 }
 
-void drawFontFaceDemo() {
-    // Font Demo1
-    // create more fonts at http://oleddisplay.squix.ch/
+float getAvgTemp() {
+  float sum = 0;
+  for(int i=0; i<NO_OF_TEMP_SENSORS; i++) { sum += temperatures[i]; }
+  return sum/NO_OF_TEMP_SENSORS;
+}
+
+String buildTemperatureString(){
+  String temperatureString = "";
+  for(int i=0; i<NO_OF_TEMP_SENSORS; i++) {
+    temperatureString += String(temperatures[i], 1);
+    if((i+1)<NO_OF_TEMP_SENSORS) {
+      temperatureString += String(", ");
+    }
+  }
+  return temperatureString;
+}
+
+void drawTemperatures() {
     Heltec.display->clear();
     Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
-    /*Heltec.display->setFont(ArialMT_Plain_10);
-    Heltec.display->drawString(0, 0, "Hello world");
-    Heltec.display->setFont(ArialMT_Plain_16);
-    Heltec.display->drawString(0, 10, "Hello world");*/
     Heltec.display->setFont(ArialMT_Plain_24);
-    Heltec.display->drawString(5, 20, "LoopDuck");
+    Heltec.display->drawString(25, 10, String(getAvgTemp(), 2) + String("C"));
     Heltec.display->setFont(ArialMT_Plain_10);
-    Heltec.display->drawString(40, 50, "is alive!");
+    Heltec.display->drawString(5, 40, buildTemperatureString());
     Heltec.display->display();
+}
+
+void readTemperatures(){ 
+  sensors.requestTemperatures();
+  delay(1); // Seems to help with random -127 readings
+  Serial.println("Readings:");
+  float smallest = 999;
+  float greatest = -999;
+  DeviceAddress tempDeviceAddress; 
+  for(int i=0;i<NO_OF_TEMP_SENSORS; i++){
+    if(sensors.getAddress(tempDeviceAddress, i)){
+      Serial.printf("%u, ", i);
+      printAddress(tempDeviceAddress);
+      float tempC = sensors.getTempC(tempDeviceAddress);
+      Serial.print("  Temp C: ");
+      Serial.println(tempC);
+      if(tempC>greatest) { greatest = tempC; }
+      if(tempC<smallest) { smallest = tempC; }
+      temperatures[i] = tempC;
+    }
+  }
+  Serial.print("Greatest diff is ");
+  Serial.print(greatest-smallest);
+  Serial.println("C");
 }
 
 void loop()
 {
-  drawFontFaceDemo();
-  // turn the LED on (HIGH is the voltage level)
-  //digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("Hello world!");
-  
-
-  // wait for a second
-  delay(1000);
-
-  // turn the LED off by making the voltage LOW
-  //digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(1000);
+  readTemperatures();
+  drawTemperatures();
+  delay(2000);
 }
