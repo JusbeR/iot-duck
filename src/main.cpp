08c3b4d791b6b941b83f6a53a3d70816234a0cbe
Jussi Ratilainen <jussi.t.ratilainen@gmail.com>
2020-08-12 19:38:27 +0300

    Adaptive reading time

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <DallasTemperature.h>
 #include "batt.h"
 
+//#define PRINT_TEMPERATURES
 #define ONE_WIRE_BUS 17
 #define NO_OF_TEMP_SENSORS 3
 #define BUTTON_PIN 18
@@ -18,14 +19,18 @@
 #define MODE_DEFAULT     MODE_SCREEN_ON
 
 #define READ_BATT_TIME_MS (30*1000)
-#define LOOP_TIME (2*1000)
-#define SKIP_BATT_READINGS (READ_BATT_TIME_MS/LOOP_TIME)
+#define LOOP_TIME_MS (500)
+#define SKIP_BATT_READINGS (READ_BATT_TIME_MS/LOOP_TIME_MS)
 // Sensors are always read in same order and this list
 // puts them so that bottom sensor is always in the first place
 // and top sensor in last
 uint8_t sensor_index_to_disp_index[3] = {1,2,0};
 
 static int skip_batt_reading_counter = 0;
+static float lastTemperature = 0.0f;
+static float temperatureChangeSinceLastReading = 0.0f;
+static int skipTemperatureReadings = 0;
+static int skipTemperatureReadingsCounter = 0;
 static volatile uint8_t screenMode;
 static volatile bool screenModeChanged;
 float temperatures[NO_OF_TEMP_SENSORS];
@@ -108,49 +113,77 @@ void draw() {
 void readTemperatures(){ 
   sensors.requestTemperatures();
   delay(1); // Seems to help with random -127 readings
+#ifdef PRINT_TEMPERATURES
   Serial.println("Readings:");
+#endif
   float smallest = 999;
   float greatest = -999;
   DeviceAddress tempDeviceAddress; 
   for(int i=0;i<NO_OF_TEMP_SENSORS; i++){
     if(sensors.getAddress(tempDeviceAddress, i)){
+#ifdef PRINT_TEMPERATURES
       Serial.printf("%u, ", i);
       printAddress(tempDeviceAddress);
+#endif
       float tempC = sensors.getTempC(tempDeviceAddress);
+#ifdef PRINT_TEMPERATURES
       Serial.print("  Temp C: ");
       Serial.println(tempC);
+#endif
       if(tempC>greatest) { greatest = tempC; }
       if(tempC<smallest) { smallest = tempC; }
       temperatures[sensor_index_to_disp_index[i]] = tempC;
     }
   }
+  float currentTemperature = getAvgTemp();
+  temperatureChangeSinceLastReading = fabs(lastTemperature-currentTemperature);
+  lastTemperature = currentTemperature;
+
+#ifdef PRINT_TEMPERATURES
   Serial.print("Greatest diff is ");
   Serial.print(greatest-smallest);
   Serial.println("C");
+#endif
+}
+
+void determineChangeRatio() {
+  if(temperatureChangeSinceLastReading > 0.08) {
+    skipTemperatureReadings = 0;
+  } else if (temperatureChangeSinceLastReading > 0.03) {
+    return;
+  } else {
+    if(skipTemperatureReadings < 20) {
+      skipTemperatureReadings++;
+    }
+  }
 }
 
 void loop()
 {
-  if(skip_batt_reading_counter++ >= SKIP_BATT_READINGS) {
+  if(skip_batt_reading_counter++ > SKIP_BATT_READINGS) {
     skip_batt_reading_counter = 0;
     sample_batt_level();
   }
-  readTemperatures();
-  if(screenMode == MODE_SCREEN_ON && screenModeChanged) {
-    Serial.print("Turning screen ON\n");
-    screenModeChanged = false;
-    // TODO: No idea how these should be used. Docs do not help.
-    Heltec.display->resetDisplay();
-    Heltec.display->init();
-    Heltec.display->displayOn();
-    Heltec.display->flipScreenVertically();
-  } else if(screenModeChanged) {
-    Serial.print("Turning screen OFF\n");
-    screenModeChanged = false;
-    Heltec.display->resetDisplay();
-    Heltec.display->sleep();
-    Heltec.display->displayOff();
+  if(skipTemperatureReadingsCounter++ > skipTemperatureReadings) {
+    readTemperatures();
+    determineChangeRatio();
+    skipTemperatureReadingsCounter = 0;
+    if(screenMode == MODE_SCREEN_ON && screenModeChanged) {
+      Serial.print("Turning screen ON\n");
+      screenModeChanged = false;
+      // TODO: No idea how these should be used. Docs do not help.
+      Heltec.display->resetDisplay();
+      Heltec.display->init();
+      Heltec.display->displayOn();
+      Heltec.display->flipScreenVertically();
+    } else if(screenModeChanged) {
+      Serial.print("Turning screen OFF\n");
+      screenModeChanged = false;
+      Heltec.display->resetDisplay();
+      Heltec.display->sleep();
+      Heltec.display->displayOff();
+    }
+    draw();
   }
-  draw();
-  delay(2000);
+  delay(LOOP_TIME_MS);
 }
